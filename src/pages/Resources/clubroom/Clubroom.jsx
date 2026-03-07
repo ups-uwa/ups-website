40a3895aafd643f79af4c62e725a5a58dc7d7c13
Zachary den Boer <zachary.denboer@gmail.com>
2026-03-07 13:48:12 +0800

    add store info

--- src/pages/Resources/clubroom/Clubroom.jsx
+++ src/pages/Resources/clubroom/Clubroom.jsx
@@ -13,6 +13,8 @@ export default function Clubroom() {
             
             <h1>UPS Clubroom</h1>
             <p>The Physics building (Room 2.19) hosts our common room. We have ping pong tables, a kitchen and ample study and lounge space for club members to enjoy.</p>
+            <h3>Store</h3>
+            <p>We run a zero-profit store, buying in bulk to supply students with the fuel to keep on studying, at the lowest prices possible. We regularly stock energy drinks, snack bars, frozen meals and more. Pay by cash or bank transfer.</p>
             
         </div>
     </div>
