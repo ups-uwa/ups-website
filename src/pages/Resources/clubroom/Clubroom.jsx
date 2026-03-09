f36de25dc1dd02f85e2230657440f24681edbc58
Zac <zachary.denboer@gmail.com>
2026-03-09 13:36:46 +0800

    add resources page

--- src/pages/Resources/clubroom/Clubroom.jsx
+++ src/pages/Resources/clubroom/Clubroom.jsx
@@ -7,11 +7,9 @@ export default function Clubroom() {
   return (
     <div className='ups__resources-clubroom'>
         <div className='ups__resources-clubroom_container'>
-            <div className='ups__resources-clubroom_placeholder'>
-              Image coming soon
-            </div>
             
             <h1>UPS Clubroom</h1>
+            <img src='assets/common-room.jpg' className='ups__resources-clubroom_photo' />
             <p>The Physics building (Room 2.19) hosts our common room. We have ping pong tables, a kitchen and ample study and lounge space for club members to enjoy.</p>
             <h3>Store</h3>
             <p>We run a zero-profit store, buying in bulk to supply students with the fuel to keep on studying, at the lowest prices possible. We regularly stock energy drinks, snack bars, frozen meals and more. Pay by cash or bank transfer.</p>
