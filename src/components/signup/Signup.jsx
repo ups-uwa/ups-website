ff509b908febeb5fc38dff5125b6a63b7d466a9c
Zac <zachary.denboer@gmail.com>
2026-03-19 13:48:08 +0800

    add config json for signup url and implement

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,10 +1,11 @@
 import "./signup.css"
+import config from "../../assets/config.json"
 
 export default function Signup() {
 
     return (
     <div className='ups__container-signup'>
-        <a href='https://docs.google.com/forms/d/e/1FAIpQLSdXBbH7limOVxM2zEMVdjtZ5pKa7uShUvHsCNf82H27INQ0lg/viewform?fbzx=4221658672631148117' target='_blank' rel="noopener noreferrer">
+        <a href= {config["signup-url"]} target='_blank' rel="noopener noreferrer">
             Sign up for an Annual Membership!
         </a>
     </div>
